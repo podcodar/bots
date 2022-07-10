053929a7701343424406b8ab26ab69bc15a13942
Marco Antônio <ma.souza.junior@gmail.com>
2022-07-09 22:41:28 -0300

    fea: make bot calculate daily points

--- entities.ts
+++ entities.ts
@@ -1,9 +1,15 @@
 export interface DailyRecord {
   userId: string;
   name: string;
+  id: string;
+  created_at: string;
 }
 
-export interface DailyScoreboard extends DailyRecord {
+export type CreateDailyRecord = Omit<DailyRecord, 'id' | 'created_at'>;
+
+export interface DailyScoreboard extends Omit<DailyRecord, 'created_at'> {
   points: number;
   currentStreak: number;
 }
+
+export type CreateDailyScoreboard = Omit<DailyScoreboard, 'id' | 'created_at'>;
--- mod.ts
+++ mod.ts
@@ -2,65 +2,44 @@ import { createBot, startBot } from "discord";
 import { bgBlue } from "colors";
 
 import { settings } from "@settings";
-import { DailyRecord, DailyScoreboard } from "@entities";
-import { addDailyRecord } from "@usecases/dailyBot.ts";
+import { addDailyRecord, computeDailyScoreboardPoint } from "@usecases/dailyBot.ts";
 
 console.log(bgBlue("Starting discord bot..."));
 
 const token = settings.DISCORD_TOKEN;
 const botId = BigInt(settings.BOT_ID);
 
-console.log("starting")
-const res = await addDailyRecord({
-  name: "test",
-  userId: '1',
-})
-console.log("ended")
-console.log(res)
-
 
 const baseBot = createBot({
   token,
   botId,
-  intents: ['Guilds', 'GuildMessages'],
+  intents: ["Guilds", "GuildMessages"],
   events: {
     ready() {
       console.log("Successfully connected to gateway");
     },
-    messageCreate(bot, message) {
+    async messageCreate(bot, message) {
       const isDailyChannel = message.channelId === BigInt(settings.DAILY_CHANNEL);
       if (!isDailyChannel) return;
 
       const content = message.content.toLocaleLowerCase();
-      const isDailyMessage =
-        content.includes("o que fiz") &&
-        content.includes("o que vou fazer");
-
+      const isDailyMessage = content.includes("o que eu fiz") && content.includes("o que vou fazer");
       if (!isDailyMessage) return;
 
       const userId = message.member!.id.toString();
       const name = message.member?.nick ?? message.tag;
-      const dailyRecord: DailyRecord = {
-        name,
-        userId,
+
+      await addDailyRecord({ name, userId });
+
+      const { error, data: dailyScoreboard } = await computeDailyScoreboardPoint(userId, name);
+      if (error != null) {
+        console.error(error);
+        return;
       }
 
-      addDailyRecord(dailyRecord);
-
-      // TODO: getScoreboardByUserId(userId)
-      // TODO: updateScoreboard(userId)
-      const dailyScoreboard: DailyScoreboard = {
-        name,
-        userId,
-        points: 0,
-        currentStreak: 0,
-      }
-
-      console.log(`content: ${message.content}`);
-      console.log({message})
-      // Process the message with your command handler here
+      console.log({ dailyScoreboard });
     },
   },
 });
 
-// await startBot(baseBot);
+await startBot(baseBot);
