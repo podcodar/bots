e4332a1ee7faa76d10f0bfb820c74655925bfd30
Marco Antônio <ma.souza.junior@gmail.com>
2022-07-19 19:23:11 -0300

    enh: improve extra point check
    - also add uniqueness check

--- services/daily-bot.go
+++ services/daily-bot.go
@@ -14,6 +14,11 @@ func ComputeDailyScoreboard(msg *discordgo.Message) {
 		Name:   msg.Author.Username,
 	}
 
+	// return if already have a daily registered
+	if repository.CountUserActivityLastDays(dailyRecord.UserId, 0) == 1 {
+		return
+	}
+
 	dailyScoreboard := repository.FindUserScoreByUserId(dailyRecord.UserId)
 	if dailyScoreboard == nil {
 		repository.AddDailyScoreboard(dailyRecord)
@@ -21,7 +26,6 @@ func ComputeDailyScoreboard(msg *discordgo.Message) {
 		dailyScoreboard.Points++
 
 		computeExtraPoints(dailyScoreboard)
-
 		computeStreak(dailyRecord.UserId, dailyScoreboard)
 
 		repository.SaveDailyScoreboard(dailyScoreboard)
@@ -29,16 +33,17 @@ func ComputeDailyScoreboard(msg *discordgo.Message) {
 	repository.AddDailyRecord(dailyRecord)
 }
 
-var REPORT_STREAK = 4
+var PREV_STANDUP_COUNT = 4
 
 func computeExtraPoints(dailyScoreboard *entities.DailyScoreboard) {
 	// add extra points if user has 5 standups on the current week
 	weekday := time.Now().Weekday()
+	hasFiveStandupsThisWeek :=
+		weekday >= time.Tuesday &&
+			repository.CountUserActivityLastDays(dailyScoreboard.UserId, int(weekday)) == PREV_STANDUP_COUNT
 
-	if weekday >= 4 {
-		if repository.CountUserActivityLastDays(dailyScoreboard.UserId, int(weekday)) == REPORT_STREAK {
-			dailyScoreboard.Points++
-		}
+	if hasFiveStandupsThisWeek {
+		dailyScoreboard.Points++
 	}
 }
 
