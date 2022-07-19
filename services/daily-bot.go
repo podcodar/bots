a9bcdd8452f22be364d97d8e058b411b1df56c90
Marco Antônio <ma.souza.junior@gmail.com>
2022-07-19 12:59:13 -0300

    enh: improve extra point processing

--- services/daily-bot.go
+++ services/daily-bot.go
@@ -3,6 +3,7 @@ package services
 import (
 	"podcodar-discord-bot/entities"
 	"podcodar-discord-bot/repository"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -19,18 +20,30 @@ func ComputeDailyScoreboard(msg *discordgo.Message) {
 	} else {
 		dailyScoreboard.Points++
 
-		// TODO: add extra points
-		// if repository.CountUserActivityLastDays(dailyRecord.UserId, 7) >= 5 {
-		// 	dailyScoreboard.Points++
-		// }
+		computeExtraPoints(dailyScoreboard)
 
-		dailyScoreboard.CurrentStreak++
-		// if no report yesterday, reset current streak
-		if repository.CountUserActivityLastDays(dailyRecord.UserId, 1) == 0 {
-			dailyScoreboard.CurrentStreak = 0
-		}
+		computeStreak(dailyRecord.UserId, dailyScoreboard)
 
 		repository.SaveDailyScoreboard(dailyScoreboard)
 	}
 	repository.AddDailyRecord(dailyRecord)
+}
+
+func computeExtraPoints(dailyScoreboard *entities.DailyScoreboard) {
+	// add extra points if user has 5 standups on the current week
+	weekday := time.Now().Weekday()
+
+	if weekday >= 4 {
+		if repository.CountUserActivityLastDays(dailyScoreboard.UserId, int(weekday)) == 5 {
+			dailyScoreboard.Points++
+		}
+	}
+}
+
+func computeStreak(userId string, dailyScoreboard *entities.DailyScoreboard) {
+	dailyScoreboard.CurrentStreak++
+	// if no report yesterday, reset current streak
+	if repository.CountUserActivityLastDays(userId, 1) == 0 {
+		dailyScoreboard.CurrentStreak = 0
+	}
 }
