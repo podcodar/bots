95a8ed018d0f6cdbf4d15ab882bd0ec6adf77dfd
Marco Antônio <ma.souza.junior@gmail.com>
2022-07-24 14:57:55 -0300

    enh: move scoreboard content to repository

--- services/show-scoreboard.go
+++ services/show-scoreboard.go
@@ -1,29 +1,11 @@
 package services
 
 import (
-	"fmt"
 	"podcodar-discord-bot/repository"
-	"strings"
-	"time"
 )
 
-var maxNameLength = 25
-
 func SendScoreboard() {
-	now := time.Now()
-	content := fmt.Sprintf("\n**Scoreboard - %s**\n\n", now.Format("02.01.2006"))
-	codeQuote := "```\n"
-	content += codeQuote
-
-	// get ranked users from scoreboard repository
-	for index, user := range repository.ScoreboardRanking(10) {
-		nameSize := len(user.Name)
-		spacesString := strings.Repeat(" ", maxNameLength-nameSize)
-		name := fmt.Sprintf("%s: %s", user.Name, spacesString)
-
-		content += fmt.Sprintf("%d. %s | %d\n", index, name, user.Points)
-	}
-	content += codeQuote
+	content := repository.CreateScoreboardContent()
 
 	// send message to daily channel
 	ds := repository.MakeDiscordSession(config.DiscordToken)
